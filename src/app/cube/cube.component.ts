5b3746e21e094757706d5cd287e8e802d8c19684
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2018-07-19 18:25:13 -0600

    rotate with arrows

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Cube } from '../cube';
 
 @Component({
@@ -8,13 +8,39 @@ import { Cube } from '../cube';
 })
 export class CubeComponent implements OnInit {
 
-  cube: Cube
+  cube: Cube;
+  rotateX: number;
+  rotateY: number;
   constructor() {
     this.cube = new Cube();
     this.cube.reset();
+    this.rotateX = -18;
+    this.rotateY = 36;
   }
 
   ngOnInit() {
   }
 
+  @HostListener('window:keydown', ['$event']) onkeyUp(event: any) {
+    if (event.keyCode === 39) {
+      this.rotateY += 5;
+    }
+    if (event.keyCode === 37) {
+      this.rotateY -= 5;
+    }
+    if (event.keyCode === 40) {
+      this.rotateX += 5;
+    }
+    if (event.keyCode === 38) {
+      this.rotateX -= 5;
+    }
+    event.stopPropagation();
+  }
+
+
+  mouseMoved(ev) {
+    let rotY = ev.pageX * 0.1;
+    let rotX = ev.pageY * 0.1;
+    //$("#cube").css("transform", "translateZ( -100px) rotateX( " + rotX + "deg) rotateY(" + rotY + "deg)");
+  }
 }
