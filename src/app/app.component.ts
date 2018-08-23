4983473a4592acf75d0dedde81acb21da2e49ec4
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2018-08-22 23:57:42 -0600

    app component doen't need a cube object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Cube } from './models';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +6,5 @@ import { Cube } from './models';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  cube: Cube;
-  constructor() {
-    this.cube = new Cube();
-  }
+  constructor() { }
 }
