ee37eabeb4f45b7e5e0253fc1ee57b1748530ab4
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2020-06-20 11:01:12 -0600

    fixing deploy file

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:crojasaragonez/vue-rubik-cube master:gh-pages
+git push -f git@github.com:crojasaragonez/angular-rubik-cube master:gh-pages
 
 cd -
